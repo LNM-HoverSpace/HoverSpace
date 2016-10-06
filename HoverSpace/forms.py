c71bbcef6d5212182aa623f8b630dd842de53697
vidhan13j07 <vidhanj1307@gmail.com>
2016-10-07 04:10:04 +0530

    small changes

--- HoverSpace/forms.py
+++ HoverSpace/forms.py
@@ -14,24 +14,4 @@ class SignUpForm(FlaskForm):
     email = StringField('Email Address', [DataRequired(), Length(min=6, max=35)])
     firstname = TextField("First name", [DataRequired()])
     lastname = TextField("Last name", [DataRequired()])
-    password = PasswordField('New Password', [DataRequired()])
-    #confirm = PasswordField('Repeat Password', [DataRequired()])
-
-    '''def __init__(self, *args, **kwargs):
-        FlaskForm.__init__(self, *args, **kwargs)
-
-    def validate(self):
-        if not FlaskForm.validate(self):
-            return False
-
-        user = USERS_COLLECTION.find_one( {"email": self.email} )
-        if user:
-            self.email.errors.append("That email is already taken!")
-            return False
-
-        user = USERS_COLLECTION.find_one( {"username": self.username} )
-        if user:
-            self.username.errors.append("That username is already taken!")
-            return False
-
-        return True'''
+    password = PasswordField('Password', [DataRequired(), Length(min=6)])
