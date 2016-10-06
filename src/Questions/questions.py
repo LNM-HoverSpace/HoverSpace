c509fecc92292cd3ea1709e117a1f15309c7d529
iamshubh22 <sgshubham03@gmail.com>
2016-10-06 08:14:01 +0530

    Add methods for posting a question

--- src/Questions/questions.py
+++ src/Questions/questions.py
@@ -1,8 +1,14 @@
-from flask import Flask
-
+from flask import Flask, request, session, redirect, render_template, url_for
 app = Flask(__name__)
 
 
-@app.route('/post-a-question')
+@app.route('/post-a-question/')
 def post_question():
+    #if request.method == 'POST':
+    #    if 'username' in session:
+    return render_template('post-a-question.html')
+    #    else:
+    #        return redirect(url_for('login'))
 
+if __name__ == '__main__':
+    app.run()
