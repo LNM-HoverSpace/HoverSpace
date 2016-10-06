cc2637c08bf402bd5d7ccac0bd60ca16844b4b8c
iamshubh22 <sgshubham03@gmail.com>
2016-10-06 05:47:27 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup ( name = 'HoverSpace',
+        version = '1.0,'
+        description = '',
+        long_description = '',
+        author = 'HoverSpace Developers',
+        author_email = '',
+        maintainer = 'Shubham Gupta, Vidhan Jain, Prashant Sharma, Shashwat Shalvi, Mohit Kumar',
+        maintainer_email = 'sgshubham03@gmail.com',
+        url = '',
+        download_url = '',
+        packagrs = [],
+        classifiers = [],
+        platforms = [],
+        license = '',
+        zip_safe = False,
+        )
