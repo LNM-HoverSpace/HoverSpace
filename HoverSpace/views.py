54e5f39ac263beae25814420d2eb6fb58af5c621
iamshubh22 <sgshubham03@gmail.com>
2016-10-25 05:22:26 +0530

    Updated files

--- HoverSpace/views.py
+++ HoverSpace/views.py
@@ -1,12 +1,14 @@
 from HoverSpace.application import app, lm
 import pymongo
 from flask import request, redirect, render_template, url_for, flash, session
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from HoverSpace.models import USERS_COLLECTION, QUESTIONS_COLLECTION, ANSWERS_COLLECTION
+from HoverSpace.QuestionClass import Question, QuestionMethods
+from HoverSpace.AnswerClass import Answers, AnswerMethods
 from HoverSpace.user import User
-from HoverSpace.forms import LoginForm, SignUpForm
+from HoverSpace.forms import LoginForm, SignUpForm, QuestionForm, AnswerForm
 from bson.objectid import ObjectId
-from HoverSpace.question import viewQuestion
+
 
 @app.route('/')
 @app.route('/home/', methods=['GET', 'POST'])
@@ -62,6 +64,46 @@ def signup():
                 flash("SignUp successfull!", category='success')
                 return redirect(url_for('login'))
     return render_template('signup.html', title='HoverSpace | Signup', form=form)
+
+@app.route('/post-a-question/', methods=['GET', 'POST'])
+@login_required
+def postQuestion():
+    form = QuestionForm()
+    if request.method == 'POST':
+        try:
+            username = current_user.get_id()
+            question = QUESTIONS_COLLECTION.find_one( {'short_description': form.short_description.data} )
+            if question:
+                flash("This queston has already been asked", category='error')
+                return render_template('post-a-question.html', title='HoverSpace | Post a Question', form=form)
+            ques_obj = Question(username, form.short_description.data, form.long_description.data)
+            quesID = ques_obj.postQuestion()
+            flash("Your question has been successfully posted.", category='success')
+            return redirect(url_for('viewQuestion', quesID=quesID))
+        except KeyError:
+            return redirect(url_for('login'))
+    return render_template('post-a-question.html', title='HoverSpace | Post a Question', form=form)
+
+
+@app.route('/question/<quesID>/', methods=['GET', 'POST'])
+@login_required
+def viewQuestion(quesID):
+    form = AnswerForm()
+    if request.method == 'POST':
+        username = current_user.get_id()
+        ans_obj = Answers(username, quesID, form.ans_text.data)
+        ansID = ans_obj.post_answer()
+
+    ques_obj = QuestionMethods(quesID)
+    ques = ques_obj.getQuestion()
+    ansmet_obj = AnswerMethods(quesID)
+    ans = ansmet_obj.get_answers(quesID)
+    return render_template('question.html', question=ques, answers=ans, form=form)
+
+@app.route('/question/<quesID>/vote/', methods=['GET', 'POST'])
+def updateVotes(quesID):
+    print("py working")
+
 
 @lm.user_loader
 def load_user(username):
